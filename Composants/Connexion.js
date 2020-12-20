4aec47441f1485946d6acec7a1d65624d7b4e48c
Anhro <khaisbeduoglu@gmail.com>
2020-12-20 15:50:18 +0100

    Front page de connexion fait logo : à modifier

--- Composants/Connexion.js
+++ Composants/Connexion.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button, Linking, Image, TouchableOpacity } from 'react-native';
 
 const Connexion = () => {
   const [value, onChangeText, mdp] = React.useState('');
@@ -7,57 +7,123 @@ const Connexion = () => {
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
-        <Text style={styles.text}>Adresse mail :</Text>
-        <TextInput
-          style={styles.inputText}
-          onChangeText={text => onChangeText(text)}
-          value={value}
-          />
-          <Text style={styles.text}>Mot de passe :</Text>
+        <View style={styles.wrapper}>
+          <Image
+          style={styles.logo}
+          source={require('../Media/Logo.png')} />
+        </View>
+        <View style={styles.wrapper1}>
+          <Text style={styles.text}>Adresse mail :</Text>
           <TextInput
             style={styles.inputText}
-            onChangeText={mdp => onChangeText(mdp)}
-            value={mdp}
+            onChangeText={text => onChangeText(text)}
+            value={value}
             />
-            <Button style={styles.button}>je suis un boutton</Button>
+            <Text style={styles.text}>Mot de passe :</Text>
+            <TextInput
+              style={styles.inputText}
+              onChangeText={mdp => onChangeText(mdp)}
+              value={mdp}
+              />
+        </View>
+        <View style={styles.wrapper2}>
+        <TouchableOpacity style={styles.buttonSecondary} title='Connexion'>
+          <Text style={styles.textSecondary}>Inscription</Text>
+        </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonPrimary} title='Connexion'>
+            <Text style={styles.textPrimary}>Connexion</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container:{ 
     flex: 1,
     backgroundColor : '#373c78',
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
   },
   innerContainer:{
-    height:'50%',
+    height:400,
     width:'75%',
     backgroundColor:'#00164c',
     justifyContent:'center',
-
+    borderRadius:3,
+  },
+  wrapper:{
+    height:130,
+    alignItems:'center',
+  },
+  wrapper1:{
+    height:160,
+    paddingTop:'5%',
+  },
+  wrapper2:{
+    paddingTop:15,
+    flexDirection:'row',
+    justifyContent:'space-around',
+    flex:1,
+  },
+  logo:{
+    resizeMode:'contain',
+    width:170,
+    height:100,
+    justifyContent:'center',
+    alignItems:'center',
+    flex:1,
+  },
+  text:{
+    color:'white',
+    paddingLeft:30,
+    fontSize:16,
   },
   inputText:{
-    height: 32,
+    height:32,
     width:'80%',
     borderColor: 'gray',
+    color:'white',
     borderWidth: 1,
     backgroundColor:'#373c78',
     padding:0,
     alignSelf:'center',
-    borderRadius:'10px'
+    borderRadius:10,
+    fontSize:16,
   },
-  text:{
+  buttonPrimary:{
+    width:100,
+    height:48,
+    backgroundColor:'#ff6b5a',
+    color:'red',
+    borderRadius:10,
+    paddingRight:'20%',
+  },
+  textPrimary:{
     color:'white',
-    paddingLeft:30
+    fontWeight:'bold',
+    fontSize:15,
+    flex:1,
+    width:100,
+    textAlign:'center',
+    paddingTop:'25%'
   },
-  button:{
-    width:50,
-    color:'black',
-  }
+  buttonSecondary:{
+    width:100,
+    height:48,
+    paddingTop:5,
+    fontSize:17,
 
+  },
+  textSecondary:{
+    color:'#ff6b5a',
+    fontWeight:'bold',
+    fontSize:14,
+    width:100,
+    textAlign:'center',
+    paddingTop:6,
+  },
 })
 
 
